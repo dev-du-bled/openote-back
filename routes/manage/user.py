3ea9c4884d5e5dcb7a4083e898db9abc36fa346c
Zarox28 <41804375+Zarox28@users.noreply.github.com>
2024-10-03 11:39:29 +0200

    Added manage user delete

--- routes/manage/user.py
+++ routes/manage/user.py
@@ -39,3 +39,34 @@ async def get_user_endp(Authorization: str = Header(...), id: int | None = None)
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="You are not allowed to access this resource",
             )
+
+
+@router.delete("/user")
+async def delete_user_endp(Authorization: str = Header(...), id: int = None):
+    conn = get_db_connection()
+    with conn.cursor(cursor_factory=RealDictCursor) as c:
+        c.execute(
+            """SELECT role FROM "user" WHERE id=(SELECT associated_user FROM "sessions" WHERE token=%s);""",
+            (Authorization,),
+        )
+        res = c.fetchone()
+
+        if res is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="No such session"
+            )
+
+        if id is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail="No user id provided"
+            )
+
+        if res.get("role") == "admin":
+            c.execute("""DELETE FROM "user" WHERE id=%s;""", (id,))
+            conn.commit()
+            return {"message": "User deleted"}
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="You are not allowed to access this resource",
+            )
