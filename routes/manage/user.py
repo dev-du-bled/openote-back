2b03b400257869c52f17e60a46d563df5c02f14c
oger-gabriel <40660493+0x4c756e61@users.noreply.github.com>
2024-10-04 14:19:31 +0200

    Add PATCH manage/user route, with partially auto-generated sql

--- routes/manage/user.py
+++ routes/manage/user.py
@@ -1,8 +1,19 @@
 from fastapi import APIRouter, Header, HTTPException, status
+from psycopg2 import errors
 from psycopg2.extras import RealDictCursor
+from pydantic import BaseModel
+
 from db import get_db_connection
 
 router = APIRouter()
+
+
+class UpdateUserData(BaseModel):
+    lastname: str | None
+    firstname: str | None
+    group: int | None
+    class_: int | None
+    student_number: int | None
 
 
 @router.get("/user")
@@ -14,7 +25,6 @@ async def get_user_endp(Authorization: str = Header(...), id: int | None = None)
             (Authorization,),
         )
         res = c.fetchone()
-
         if res is None:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND, detail="No such session"
@@ -30,7 +40,8 @@ async def get_user_endp(Authorization: str = Header(...), id: int | None = None)
 
             if res is None:
                 raise HTTPException(
-                    status_code=status.HTTP_404_NOT_FOUND, detail="No such user"
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail="No such user or no users",
                 )
 
             return res
@@ -41,7 +52,7 @@ async def get_user_endp(Authorization: str = Header(...), id: int | None = None)
             )
 
 
-@router.delete("/user")
+@router.delete("/user", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_user_endp(Authorization: str = Header(...), id: int = None):
     conn = get_db_connection()
     with conn.cursor(cursor_factory=RealDictCursor) as c:
@@ -64,9 +75,100 @@ async def delete_user_endp(Authorization: str = Header(...), id: int = None):
         if res.get("role") == "admin":
             c.execute("""DELETE FROM "user" WHERE id=%s;""", (id,))
             conn.commit()
-            return {"message": "User deleted"}
+            return
+
         else:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="You are not allowed to access this resource",
+            )
+
+
+@router.patch(
+    "/user", name="Change a user info", status_code=status.HTTP_204_NO_CONTENT
+)
+async def update_usr_endp(
+    user_data: UpdateUserData, Authorization: str = Header(...), id: int = None
+):
+    conn = get_db_connection()
+    with conn.cursor(cursor_factory=RealDictCursor) as c:
+        print(user_data)
+        c.execute(
+            """SELECT role FROM "user" WHERE id=(SELECT associated_user FROM "sessions" WHERE token=%s);""",
+            (Authorization,),
+        )
+        res = c.fetchone()
+
+        if res is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="No such session"
+            )
+
+        if id is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail="No user id provided"
+            )
+
+        if res.get("role") != "admin":
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="You are not allowed to access this resource",
+            )
+
+        fields = map(
+            lambda n: '"' + (n[0:-1] if n[-1] == "_" else n) + '"',
+            list(UpdateUserData.__fields__.keys()),
+        )
+
+        fields = list(fields)
+        print(fields)
+
+        selected_fields = ",".join(list(fields))
+
+        # Would have been usefull if postgres allowed to edit multiple tables in one go
+
+        # edit_fields = ",".join(
+        #     list(
+        #         map(
+        #             lambda n: n + '"=%s',
+        #             fields,
+        #         )
+        #     )
+        # )
+
+        c.execute(
+            f"""SELECT {selected_fields} FROM "user" LEFT OUTER JOIN student_info ON student_info.user_id="user".id  WHERE "user".id=%s;""",
+            (id,),
+        )
+
+        old_data = c.fetchone()
+
+        new_data = list()
+
+        for i, field_name in enumerate(list(fields)):
+            f_name = field_name.replace('"', "")
+            field_value = (
+                dict(user_data)[list(UpdateUserData.__fields__.keys())[i]]
+                or old_data[f_name]
+            )
+            new_data.append(field_value)
+
+        new_data = tuple(new_data)
+        # Thanks postgres for ruining my fun by nyot allowing me to update accross multiple table in one row
+        print((new_data[2:] + (id,)))
+        try:
+            c.execute(
+                """UPDATE "user" SET lastname=%s,firstname=%s WHERE id=%s;""",
+                (new_data[0:2] + (id,)),
+            )
+            c.execute(
+                """UPDATE "student_info" SET "group"=%s,"class"=%s,"student_number"=%s WHERE user_id=%s;""",
+                (new_data[2:] + (id,)),
+            )
+
+            conn.commit()
+        except errors.ForeignKeyViolation:
+            raise HTTPException(
+                status_code=status.HTTP_406_NOT_ACCEPTABLE,
+                detail="No such group or class",
             )
