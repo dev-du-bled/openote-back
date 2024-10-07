4d04f7985c40ce0e76369775364a118be6ee2efa
Zarox28 <41804375+Zarox28@users.noreply.github.com>
2024-10-07 10:03:11 +0200

    Fixed

--- routes/planning.py
+++ routes/planning.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Header, HTTPException, status
+from psycopg2.extras import RealDictCursor
 from pydantic import BaseModel
+from db import get_db_connection
 
 
 class Event(BaseModel):
@@ -12,12 +14,15 @@ router = APIRouter()
 
 @router.get("/planning", name="Get all events")
 async def get_planning_endp(event: Event, Authorization: str = Header(...)):
-    if event.start is None or event.end is None:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Both start and end must be provided",
-        )
+    conn = get_db_connection()
+    with conn.cursor(cursor_factory=RealDictCursor) as c:
+        c.execute("""SELECT token FROM sessions WHERE token=%s;""", (Authorization,))
+        res = c.fetchone()
+        if res is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="No such session"
+            )
 
-    raise HTTPException(
-        status_code=status.HTTP_501_NOT_IMPLEMENTED, detail="Not implemented"
-    )
+        raise HTTPException(
+            status_code=status.HTTP_501_NOT_IMPLEMENTED, detail="Not implemented"
+        )
