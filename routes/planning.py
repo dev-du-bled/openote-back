04ed93d76601f49f42e97c2958b2fdaecf42e5a6
Zarox28 <41804375+Zarox28@users.noreply.github.com>
2024-10-07 10:00:09 +0200

    Fixed

--- routes/planning.py
+++ routes/planning.py
@@ -12,6 +12,12 @@ router = APIRouter()
 
 @router.get("/planning", name="Get all events")
 async def get_planning_endp(event: Event, Authorization: str = Header(...)):
+    if event.start is None or event.end is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Both start and end must be provided",
+        )
+
     raise HTTPException(
         status_code=status.HTTP_501_NOT_IMPLEMENTED, detail="Not implemented"
     )
