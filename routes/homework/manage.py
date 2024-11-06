97aa78241d1798acd52425ad64708f99f6b3c2c7
Zarox28 <41804375+Zarox28@users.noreply.github.com>
2024-11-06 08:49:08 +0100

    Added patch homework

--- routes/homework/manage.py
+++ routes/homework/manage.py
@@ -3,6 +3,7 @@ from psycopg2 import errors
 from psycopg2.extras import RealDictCursor
 from pydantic import BaseModel
 
+import utils.autogen as gen
 import utils.ensurances as ens
 from db import get_db_connection
 
@@ -58,5 +59,39 @@ async def delete_homework_endp(id: int, Authorization: str = Header(...)):
         c.execute("DELETE FROM homework_status WHERE homework=%s;", (id,))
 
         c.execute("DELETE FROM assigned_homework WHERE id=%s;", (id,))
+
+        conn.commit()
+
+
+@router.patch(
+    "/manage", name="Update a homework", status_code=status.HTTP_204_NO_CONTENT
+)
+async def update_homework_endp(
+    id: int, homework: Homework, Authorization: str = Header(...)
+):
+    conn = get_db_connection()
+    with conn.cursor(cursor_factory=RealDictCursor) as c:
+        role = ens.get_role_from_token(c, Authorization)
+
+        ens.ensure_user_is_role(role, ens.UserRole.teacher)
+
+        fields = gen.get_obj_fields(Homework)
+        c.execute(
+            f"SELECT {gen.format_fields_to_select_sql(fields)} FROM assigned_homework WHERE id=%s;",
+            (id,),
+        )
+
+        old_data = c.fetchone()
+        if old_data is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="No such homework"
+            )
+
+        new_data = gen.merge_data(Homework, old_data, homework)
+
+        c.execute(
+            f"""UPDATE assigned_homework SET {gen.format_fields_to_update_sql(fields)} WHERE id=%s;""",
+            (new_data + (id,)),
+        )
 
         conn.commit()
