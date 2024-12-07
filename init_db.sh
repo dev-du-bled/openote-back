8ab58ec98309bb1aa90ade7cd4378cd5f249c0de
Luna <40660493+0x4c756e61@users.noreply.github.com>
2024-12-07 16:06:16 +0100

    Remove base sql from init_db.sh

--- init_db.sh
+++ init_db.sh
@@ -1,4 +1,3 @@
 mkdir -p storage/logos/ && cp -a assets/logos/. storage/logos/
 
-docker exec openote_db psql -U openuser -d openote --password -p 5432 -h localhost -f /app/base.sql
 docker exec openote_db psql -U openuser -d openote --password -p 5432 -h localhost -f /app/entries.sql
