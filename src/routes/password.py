b0d26446124c6a8a625f89dfc78ad4a3f75dc39c
Zarox28 <41804375+Zarox28@users.noreply.github.com>
2025-01-15 11:49:53 +0100

    Ruff format

--- src/routes/password.py
+++ src/routes/password.py
@@ -9,35 +9,34 @@ from db import Database
 
 
 class UpdatePassword(BaseModel):
-  old_password: str
-  new_password: str
+    old_password: str
+    new_password: str
+
 
 router = APIRouter()
 db = Database()
 
+
 @router.patch("/password", name="Update user password")
 async def update_password_endp(data: UpdatePassword, Authorization: str = Header(...)):
-  conn = db.get_connection()
-  with conn.cursor(cursor_factory=RealDictCursor) as c:
-    user_id = ens.get_user_col_from_token(c, "id", Authorization)
+    conn = db.get_connection()
+    with conn.cursor(cursor_factory=RealDictCursor) as c:
+        user_id = ens.get_user_col_from_token(c, "id", Authorization)
 
-    c.execute(
-      """SELECT password_hash FROM "user" WHERE id=%s;""",
-      (user_id,)
-    )
-    res = c.fetchone()
+        c.execute("""SELECT password_hash FROM "user" WHERE id=%s;""", (user_id,))
+        res = c.fetchone()
 
-    if res is None:
-      raise HTTPException(status_code=404, detail="No such user !")
+        if res is None:
+            raise HTTPException(status_code=404, detail="No such user !")
 
-    if hs.sha256(data.old_password.encode()).hexdigest() != res["password_hash"]:
-      raise HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Wrong password !",
-      )
+        if hs.sha256(data.old_password.encode()).hexdigest() != res["password_hash"]:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Wrong password !",
+            )
 
-    c.execute(
-      """UPDATE "user" SET password_hash=%s WHERE id=%s;""",
-      (hs.sha256(data.new_password.encode()).hexdigest(), user_id)
-    )
-    conn.commit()
+        c.execute(
+            """UPDATE "user" SET password_hash=%s WHERE id=%s;""",
+            (hs.sha256(data.new_password.encode()).hexdigest(), user_id),
+        )
+        conn.commit()
