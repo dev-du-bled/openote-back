edbff88bacca35e2d2b20e0ac385da645c5d7d2b
Luna <40660493+0x4c756e61@users.noreply.github.com>
2025-01-22 11:20:57 +0100

    Fix text logo paths

--- scripts/setup_debug_entries.sh
+++ scripts/setup_debug_entries.sh
@@ -1,2 +1,2 @@
 docker exec openote_db psql -U openuser -d openote --password -p 5432 -h localhost -f /app/entries.sql
-docker exec openote_api cp -r /app/default/logos /app/storage/logos
+docker exec openote_api cp -r /app/default/logos /app/storage/
